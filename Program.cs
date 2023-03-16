d8ab1a3803906192e24ebe796ae407c7b60c17c8
Krenar Manxhuka <manxhuka01@hotmail.com>
2023-03-16 21:08:37 +0100

    batman

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Master Wayne.");
 
         CommandLineInterface.Command(args);
     }
